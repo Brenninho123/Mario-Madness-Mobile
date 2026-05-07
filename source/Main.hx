60f8727476b0c66ab40648fb8f15c483d2bc90c4
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-07 16:24:04 -0300

    Refactor Main class and clean up imports

--- source/Main.hx
+++ source/Main.hx
@@ -9,18 +9,8 @@ import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.display.StageScaleMode;
 import openfl.events.Event;
-import openfl.events.FocusEvent;
 import openfl.system.System;
-import lime.system.System as LimeSystem;
-
-#if mobile
-import mobile.backend.MobileScaleMode;
-#if android
-import mobile.backend.StorageUtil;
-#end
-#end
 
 #if cpp
 import cpp.vm.Gc;
@@ -32,55 +22,53 @@ import java.vm.Gc;
 import neko.vm.Gc;
 #end
 
-class Main extends Sprite
-{
+class Main extends Sprite {
 	public static var initialState:Class<FlxState> = TitleState;
-	public static var gameWidth:Int  = 1280;
+	public static var gameWidth:Int = 1280; 
 	public static var gameHeight:Int = 720;
-
-	var zoom:Float       = -1;
-	var framerate:Int    = 60;
-	var skipSplash:Bool  = true;
+	var zoom:Float = -1;
+	var framerate:Int = 60;
+	var skipSplash:Bool = true;
 	var startFullscreen:Bool = false;
 
 	public static var fpsVar:FPS;
+	public static var skipNextDump:Bool = false;
+	public static var forceNoVramSprites:Bool = #if mobile true #else false #end;
 
-	public static var skipNextDump:Bool      = false;
-	public static var forceNoVramSprites:Bool = #if (desktop && !web) false #else true #end;
-
-	public static function main():Void
-	{
+	public static function main():Void {
 		Lib.current.addChild(new Main());
 	}
 
-	public function new()
-	{
+	public function new() {
 		super();
 
-		#if android
-		StorageUtil.requestPermissions();
-		#end
-
-		#if mobile
-		Sys.setCwd(StorageUtil.getStorageDirectory());
-		#end
-
-		if (stage != null)
+		if (stage != null) {
 			init();
-		else
+		} else {
 			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
 	}
 
-	private function init(?e:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
+	private function init(?E:Event):Void {
+		if (hasEventListener(Event.ADDED_TO_STAGE)) {
 			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
 
 		setupGame();
 	}
 
-	public function setupGame():Void
-	{
+	public function setupGame():Void {
+		#if mobile
+		FlxG.signals.gameResized.add(function(w:Int, h:Int) {
+			if (FlxG.cameras != null) {
+				for (cam in FlxG.cameras.list) {
+					if (cam != null && cam.filters != null)
+						fixShaderSize(cam.flashSprite);
+				}
+			}
+		});
+		#end
+
 		Lib.application.window.onClose.add(PlayState.onWinClose);
 
 		#if !debug
@@ -89,104 +77,50 @@ class Main extends Sprite
 
 		FlxTransitionableState.skipNextTransOut = true;
 
+		var game = new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen);
+		addChild(game);
+
 		#if !mobile
 		fpsVar = new FPS(10, 4, 0xFFFFFF);
-		if (fpsVar != null) fpsVar.visible = false;
+		addChild(fpsVar);
 		#end
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
-
-		Lib.current.stage.align     = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-
-		#if mobile
-		LimeSystem.allowScreenTimeout = false;
-		FlxG.scaleMode = new MobileScaleMode();
-		#end
-
-		#if android
-		FlxG.android.preventDefaultKeys = [BACK];
-		#end
-
-		#if html5
-		FlxG.autoPause     = false;
-		FlxG.mouse.visible = false;
-		#end
-
-		FlxG.signals.preStateSwitch.add(function()
-		{
-			if (!Main.skipNextDump)
-			{
+		FlxG.signals.preStateSwitch.add(function() {
+			if (!Main.skipNextDump) {
 				Paths.clearStoredMemory(true);
 				FlxG.bitmap.dumpCache();
 			}
 			clearMajor();
 		});
 
-		FlxG.signals.postStateSwitch.add(function()
-		{
+		FlxG.signals.postStateSwitch.add(function() {
 			Paths.clearUnusedMemory();
 			clearMajor();
 			Main.skipNextDump = false;
 		});
 
-		#if !mobile
-		addChild(fpsVar);
+		#if html5
+		FlxG.autoPause = false;
 		#end
-
-		_setupFocusHandlers();
-
-		FlxG.signals.gameResized.add(onResizeGame);
+		
+		#if mobile
+		FlxG.mouse.visible = false;
+		#end
 	}
 
-	function _setupFocusHandlers():Void
-	{
-		Lib.current.stage.addEventListener(FocusEvent.FOCUS_IN, function(_)
-		{
-			#if mobile
-			LimeSystem.allowScreenTimeout = false;
-			#end
-			FlxG.game.focusLostFramerate = framerate;
-		});
-
-		Lib.current.stage.addEventListener(FocusEvent.FOCUS_OUT, function(_)
-		{
-			#if mobile
-			LimeSystem.allowScreenTimeout = true;
-			#end
-			FlxG.game.focusLostFramerate = 10;
-		});
-	}
-
-	function onResizeGame(w:Int, h:Int):Void
-	{
-		fixShaderSize(this);
-		if (FlxG.game != null) fixShaderSize(FlxG.game);
-		if (FlxG.cameras == null) return;
-
-		for (cam in FlxG.cameras.list)
-		{
-			@:privateAccess
-			if (cam != null && cam._filters != null)
-				fixShaderSize(cam.flashSprite);
+	function fixShaderSize(sprite:Sprite) {
+		@:privateAccess {
+			if (sprite != null) {
+				sprite.__cacheBitmap = null;
+				sprite.__cacheBitmapData = null;
+				sprite.__cacheBitmapData2 = null;
+				sprite.__cacheBitmapData3 = null;
+				sprite.__cacheBitmapColorTransform = null;
+			}
 		}
 	}
 
-	function fixShaderSize(sprite:Sprite):Void
-	{
-		if (sprite == null) return;
-		@:privateAccess
-		{
-			sprite.__cacheBitmap              = null;
-			sprite.__cacheBitmapData          = null;
-			sprite.__cacheBitmapData2         = null;
-			sprite.__cacheBitmapData3         = null;
-			sprite.__cacheBitmapColorTransform = null;
-		}
-	}
-
-	public static function clearMajor():Void
-	{
+	public static function clearMajor() {
 		#if cpp
 		Gc.run(true);
 		Gc.compact();
